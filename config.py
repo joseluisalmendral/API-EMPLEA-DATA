b75dab8c9873eed4e10d0e63435b4a56d912cb1b
joseluisalmendral <joseluisfunnels@gmail.com>
2025-02-04 14:27:30 +0000

    done

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 # Intentar obtener las variables de entorno usando os.environ.get
-DB_COLLECTION = os.environ.get("DB_COLLECTION", "MISSING")
 DB_NAME = os.environ.get("DB_NAME", "MISSING")
+DB_COLLECTION = os.environ.get("DB_COLLECTION", "MISSING")
 DB_PASS = os.environ.get("DB_PASS", "MISSING")
