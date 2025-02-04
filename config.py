e83a8d907e1c256bd5e57e4a463cad1069b900c7
joseluisalmendral <joseluisfunnels@gmail.com>
2025-02-04 13:27:31 +0000

    done

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ DB_NAME = os.environ.get("DB_NAME", "MISSING")
 DB_COLLECTION = os.environ.get("DB_COLLECTION", "MISSING")
 
 # Imprimir las variables para depuración
-print(f"🔍 DEBUG: DB_PASS={DB_PASS}, DB_NAME={DB_NAME}, DB_COLLECTION={DB_COLLECTION}")
+print(f"🔍 DEBUG xxx: DB_PASS={DB_PASS}, DB_NAME={DB_NAME}, DB_COLLECTION={DB_COLLECTION}")
 
 # Validar que las variables de entorno existan
-if "MISSING" in [DB_PASS, DB_NAME, DB_COLLECTION]:
-    raise ValueError("❌ ERROR: Faltan variables de entorno requeridas en Render")
+# if "MISSING" in [DB_PASS, DB_NAME, DB_COLLECTION]:
+#     raise ValueError("❌ ERROR: Faltan variables de entorno requeridas en Render")
